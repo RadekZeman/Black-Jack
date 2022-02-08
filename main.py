bc9b22f083a8e6d71d7a9aee5ac8af5dadb9e480
ravycz <radek2005@centrum.cz>
2022-02-08 17:41:55 +0100

    asi hotovo

--- main.py
+++ main.py
@@ -44,16 +44,46 @@ def double(hrac):
     return hrac
 
 
+def vyhodnoceni(hrac, krupierbody,):
+    hracbody = sum(hrac)
+    if hracbody > 21:
+        for i in hrac:
+            if i == 11:
+                hracbody -= 10
+                break
+    if hracbody > 21:
+        print(f"prohra")
+    elif krupierbody > 21:
+        print(f"vyhra")
+    elif hracbody > krupierbody:
+        print(f"vyhra")
+    elif hracbody == krupierbody:
+        print(f"remiza")
+    else:
+        print(f"prohra")
+
+
 if __name__ == '__main__':
     karty = [2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10,
              10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 11, 11, 11, 11]
     random.shuffle(karty)
     protivnik = karty[0]
-    print(karty)
     hrac = [karty[1], karty[2]]
     a = 3
     print(hrac)
     print(f"krupierova prvni karta je {protivnik}")
+    c = 1
+    b = 1
+    d = 1
+    pojistka = 0
+    if protivnik == 11:
+        text2 = """
+                   Muzes koupit pojistku- vsadit na to, ze krupier dostane Blackjack.
+                   0 = nechci kooupit
+                   1 = chci koupit
+                   """
+        pojistka = input("pojistka:")
+        pojistka = int(pojistka)
     text1 = """
            moznosti hrace:
            0 = Stand: Zůstat stát, ukončit hru.
@@ -61,7 +91,6 @@ if __name__ == '__main__':
            2 = Double: Zdvojnásobit vsazenou částku.Hrac dostane od krupiéra ještě jednu kartu a hra končí.
            3 = Split: Rozdělit hru.
            4 = Vzdat hru: Hrac ztrácí polovinu vsazené částky.
-           5 = Pojisteni: Je možné vsadit sázku na to, že krupiér dosáhne black jack.
            """
     print(text1)
     rozhodnuti = input("tvoje rozhodnuti (cislo):")
@@ -89,17 +118,41 @@ if __name__ == '__main__':
             if rozhodnutihrace1 == 2:
                 double(hrac1)
                 a += 1
+            if rozhodnutihrace1 == 4:
+                print(f"s hracem 1 jsi se vzdal")
+            c = 0
             if rozhodnutihrace2 == 1:
                 hrac1, a = hit(hrac2, a)
             if rozhodnutihrace2 == 2:
                 double(hrac2)
                 a += 1
+            if rozhodnutihrace1 == 4:
+                print(f"s hracem 2 jsi se vzdal")
+            d = 0
             break
     if rozhodnuti == 1:
         hrac, a = hit(hrac, a)
     if rozhodnuti == 2:
         double(hrac)
         a += 1
-    x = krupier(karty, protivnik, a)
-    body = sum(x)
-    print(f"krupieruv soucet je {body}")
+    if rozhodnuti == 4:
+        print(f"vzdal ses")
+        b = 0
+    if b:
+        x = krupier(karty, protivnik, a)
+        krupierbody = sum(x)
+        print(f"krupieruv soucet je {krupierbody}")
+        if rozhodnuti == 3:
+            if c:
+                print(f"vyhodnoceni hrace 1")
+                vyhodnoceni(hrac1, krupierbody)
+            if d:
+                print(f"vyhodnoceni hrace 2")
+                vyhodnoceni(hrac2, krupierbody)
+        else:
+            vyhodnoceni(hrac, krupierbody)
+        if pojistka:
+            if krupierbody == 21:
+                print(f"pojistka vyhra")
+            else:
+                print(f"pojistka prohra")
